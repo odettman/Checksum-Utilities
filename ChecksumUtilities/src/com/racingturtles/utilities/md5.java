c9ca75b01e4cf3a964685a2e58afe8508363d24d
Oliver Dettman <github@racingturtles.com>
2011-07-15 15:27:29 -0400

    Check arguments. Output usage if incorrect.

--- ChecksumUtilities/src/com/racingturtles/utilities/md5.java
+++ ChecksumUtilities/src/com/racingturtles/utilities/md5.java
@@ -12,23 +12,29 @@ public class md5 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Usage: md5 filename");
+			System.exit(1);
+		}
+
 		String filename = args[0];
 		String output = md5(filename);
 		System.out.println("MD5 Hash: " + output);
-		
+
+
 
 	}
 	public static String md5(String fileName) {
 		FileInputStream subjectFile = null;
 		String md5Hash = "Not Calculated";
-		
+
 		try {
 			subjectFile = new FileInputStream(fileName);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		try {
 			md5Hash = DigestUtils.md5Hex(subjectFile);
 		} catch (NullPointerException npe) {
@@ -37,14 +43,14 @@ public class md5 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		try {
 			subjectFile.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return md5Hash;
 	}
 }
